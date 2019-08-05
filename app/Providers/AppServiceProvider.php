22172e2f56fb2ea4e8106a2b210c753bb38203c6
prodinic <seonggyu@magice.co>
2019-08-05 17:51:59 +0900

    fix error for setting aurora

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,26 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\facades\schema;
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+   /**
+    * Register any application services.
+    *
+    * @return void
+    */
+   public function register()
+   {
+       //
+   }
+   /**
+    * Bootstrap any application services.
+    *
+    * @return void
+    */
+   public function boot()
+   {
+       //
+       Schema::defaultStringLength(191);
+   }
 }
