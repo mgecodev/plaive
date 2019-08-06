5eaf7ed80fd1c9615505af71a5e028500380c0c3
prodinic <seonggyu@magice.co>
2019-08-06 10:04:08 +0900

    delete code for authentication

--- app/Account.php
+++ app/Account.php
@@ -6,7 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Account extends Authenticatable implements MustVerifyEmail
+class Account extends Authenticatable
 {
     use Notifiable;
 
