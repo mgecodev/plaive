34f339678e1516221cfd9c668f0b9ed9b09897d7
momster444 <momster444@naver.com>
2019-08-28 15:36:29 +0900

    correct error

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -105,9 +105,7 @@
             <span class="fa fa-angle-left" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
-        @endif
         <!-- Right Control -->
-        @if(!(new \Jenssegers\Agent\Agent())->isPhone())
         <a class="new-effect carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
             <span class="fa fa-angle-right" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
