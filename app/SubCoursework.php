686829358fad2c4d29032fe622ced630ef1eba74
prodinic <seonggyu@magice.co>
2019-08-30 09:45:04 +0900

    change code for error

--- app/SubCoursework.php
+++ app/SubCoursework.php
@@ -6,21 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SubCoursework extends Model
 {
-<<<<<<< HEAD
-
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
-    protected $table = 'SubCourseworks';
-
-    protected $fillable = [
-        'SubCourseworkId', 'CourseworkId', 'Content'
-    ];
-=======
     public $primaryKey = 'SubCourseworkId';
     protected $table = 'SubCourseworks';
     protected $guarded = [];
->>>>>>> 544e4c0c63e35ec32908581dc8841546a6415a17
 }
